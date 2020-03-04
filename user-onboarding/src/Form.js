38046d6470bbb30ba1584f47c7fcad94926b8c16
Courtney Buratto <cmburatto@gmail.com>
2020-03-04 08:43:02 -0700

    wired up form to formik

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,16 +1,28 @@
-import React from 'react'
-import { withFormik, Form, Field } from 'formik'
+import React from "react";
+import { withFormik, Form, Field } from "formik";
 
-const Form = () => {
-	return (
-		<div className='form'>
-			<Form>
-				<Field type='text' name='username' placeholder='Username' />
-				<Field type='password' name='password' placeholder='Password' />
-				<button>Submit!</button>
-			</Form>
-		</div>
-	)
+function Form() {
+  return (
+    <Form>
+      <Field type="email" name="email" placeholder="Email" />
+      <Field type="password" name="password" placeholder="Password" />
+      <button>Submit!</button>
+    </Form>
+  );
 }
 
-export default Form
+const FormikLoginForm = withFormik({
+  mapPropsToValues({ email, password }) {
+    return {
+      email: email || "",
+      password: password || ""
+    };
+  },
+
+  handleSubmit(values) {
+    console.log(values);
+    //THIS IS WHERE YOU DO YOUR FORM SUBMISSION CODE... HTTP REQUESTS, ETC.
+  }
+})(Form);
+
+export default FormikForm;
