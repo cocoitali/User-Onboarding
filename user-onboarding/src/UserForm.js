c274084e88449d062d2db09a5972ef105c33419c
Courtney Buratto <cmburatto@gmail.com>
2020-03-04 12:41:15 -0700

    Verified successful POST, 201

--- user-onboarding/src/UserForm.js
+++ user-onboarding/src/UserForm.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { withFormik, Form, Field, ErrorMessage } from 'formik'
 import * as Yup from 'yup'
+import axios from "axios";
 
 function UserForm({ errors, touched, values }) {
 	return (
@@ -48,10 +49,24 @@ const FormikUserForm = withFormik({
 			.required('Please check the TOS box to submit the form')
 	}),
 
-	handleSubmit(values) {
+	handleSubmit(values, { resetForm, setErrors, setSubmitting }) {
 		console.log(values)
-		//THIS IS WHERE YOU DO YOUR FORM SUBMISSION CODE... HTTP REQUESTS, ETC.
-	}
+		// if (values.email === "alreadytaken@atb.dev") {
+		//   setErrors({ email: "That email is already taken" });
+		// } else {
+		  axios
+			.post("https://reqres.in/api/users", values)
+			.then(res => {
+			  console.log(res); // Data was created successfully and logs to console
+			  resetForm();
+			  setSubmitting(false);
+			})
+			.catch(err => {
+			  console.log(err); // There was an error creating the data and logs to console
+			  setSubmitting(false);
+			});
+		}
+	// }
 })(UserForm)
 
 export default FormikUserForm
