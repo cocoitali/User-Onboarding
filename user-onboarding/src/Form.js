5a468b78c1df36d4481f0ec9cb87bc7e373fda63
Courtney Buratto <cmburatto@gmail.com>
2020-03-04 09:14:30 -0700

    Connect validation to email field

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,38 +1,41 @@
-import React from "react";
-import { withFormik, Form, Field } from "formik";
-import * as Yup from "yup";
+import React from 'react'
+import { withFormik, Form, Field, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
 
 function Form() {
-  return (
-    <Form>
-      <Field type="email" name="email" placeholder="Email" />
-      <Field type="password" name="password" placeholder="Password" />
-      <button>Submit!</button>
-    </Form>
-  );
+	return (
+		<Form>
+			<div>
+				{touched.email && errors.email && <p>{errors.email}</p>}
+				<Field type='email' name='email' placeholder='Email' />
+				<Field type='password' name='password' placeholder='Password' />
+				<button>Submit!</button>
+			</div>
+		</Form>
+	)
 }
 
 const FormikLoginForm = withFormik({
-  mapPropsToValues({ email, password }) {
-    return {
-      email: email || "",
-      password: password || ""
-    };
-  },
+	mapPropsToValues({ email, password }) {
+		return {
+			email: email || '',
+			password: password || ''
+		}
+	},
 
-  validationSchema: Yup.object().shape({
-    email: Yup.string()
-      .email()
-      .required(),
-    password: Yup.string()
-      .min(6)
-      .required()
-  }),
+	validationSchema: Yup.object().shape({
+		email: Yup.string()
+			.email()
+			.required(),
+		password: Yup.string()
+			.min(6)
+			.required()
+	}),
 
-  handleSubmit(values) {
-    console.log(values);
-    //THIS IS WHERE YOU DO YOUR FORM SUBMISSION CODE... HTTP REQUESTS, ETC.
-  }
-})(Form);
+	handleSubmit(values) {
+		console.log(values)
+		//THIS IS WHERE YOU DO YOUR FORM SUBMISSION CODE... HTTP REQUESTS, ETC.
+	}
+})(Form)
 
-export default FormikForm;
+export default FormikForm
